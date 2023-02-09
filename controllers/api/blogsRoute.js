87ca9e164c2a1c25489cd3c7352362bb30285276
Mona Lisa <cjensen@netechnical.com>
2023-02-08 18:14:00 -0600

    added a get request to the blogsRoute, and made minor edits to main.handlebars

--- controllers/api/blogsRoute.js
+++ controllers/api/blogsRoute.js
@@ -1,6 +1,17 @@
 const router = require('express').Router();
-const { Blog } = require('../../models');
+const { Blog, User } = require('../../models');
 const withAuth = require('../../utils/auth');
+
+router.get('/', async (req, res) => {
+  try {
+    const allBlogs = await Blog.findAll({
+      include: [{ model: User }]
+    });
+    res.status(200).json(allBlogs);
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 router.post('/', withAuth, async (req, res) => {
   const body = req.body;
