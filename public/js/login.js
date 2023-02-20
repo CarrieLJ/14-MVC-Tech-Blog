834ec5ba94ba56c1b6d0dcd36f52cacabc4acb2f
Mona Lisa <cjensen@netechnical.com>
2023-02-20 10:05:59 -0600

    added code

--- public/js/login.js
+++ public/js/login.js
@@ -2,18 +2,19 @@ const loginFormHandler = async (event) => {
     event.preventDefault();
   
     // Collect values from the login form
+    const username = document.querySelector('#username-login').value.trim();
     const password = document.querySelector('#password-login').value.trim();
   
-    if (password) {
+    if (username && password) {
       // Send a POST request to the API endpoint
       const response = await fetch('/api/users/login', {
         method: 'POST',
-        body: JSON.stringify({ password }),
+        body: JSON.stringify({ username, password }),
         headers: { 'Content-Type': 'application/json' },
       });
   
       if (response.ok) {
-        document.location.replace('/login');
+        document.location.replace('/homepage');
       } else {
         alert(response.statusText);
       }
