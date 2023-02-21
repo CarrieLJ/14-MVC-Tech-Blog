07aaa05a0cef16259293971d6c21bc64a513cdbd
Mona Lisa <cjensen@netechnical.com>
2023-02-21 15:41:03 -0600

    created newblog.handlebars (but maybe I don't need that..), moved code around between dashboard and addblog handlebars

--- controllers/dashboardRoute.js
+++ controllers/dashboardRoute.js
@@ -2,20 +2,20 @@ const router = require('express').Router();
 const { Blog } = require('../models');
 const withAuth = require('../utils/auth');
 
-router.get('/', withAuth, async (req, res) => {
+router.get('/', async (req, res) => {
     try {
-        const usersBlogs = await Blog.findAll();
-        res.status(200).json(usersBlogs);
+        const allBlogs = await Blog.findAll();
+        res.status(200).json(allBlogs);
     } catch (err) {
         res.status(500).json(err);
     }
 });
 
-router.get('/blog', withAuth, async (req, res)=> {
-  let allBlogsByUser = await Blog.findAll({
-    // where: {user_id: req.session.user_id}
-  });
-  let blogData = allBlogsByUser.map((blog)=> blog.get({plain: true}));
+// router.get('/blog', withAuth, async (req, res)=> {
+//   let allBlogsByUser = await Blog.findAll({
+//     // where: {user_id: req.session.user_id}
+//   });
+//   let blogData = allBlogsByUser.map((blog)=> blog.get({plain: true}));
 
-  res.render('dashboard', {blogData});
-});
+//   res.render('dashboard', {blogData});
+// });
