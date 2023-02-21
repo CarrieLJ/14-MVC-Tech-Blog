f4c92bf59fee51b05af5c4be98239c294833a9c9
Mona Lisa <cjensen@netechnical.com>
2023-02-21 14:13:05 -0600

    fixed route for dashboard to render layout

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -69,15 +69,18 @@ router.get('/login', (req, res) => {
   res.render('login');
 });
 
-// router.get('/dashboard', (req, res) => {
-//   // If the user is already logged in, redirect the request to another route
-//   if (req.session.logged_in) {
-//     res.redirect('/');
-//     return;
-//   }
+router.get('/dashboard', (req, res) => {
+  // If the user is already logged in, redirect the request to another route
+  if (req.session.logged_in) {
+    res.redirect('/');
+    return;
+  }
 
-//   res.render('dashboard');
-// });
+  res.render('addblog', {
+    layout: 'dashboard',
+
+  });
+});
 
 router.get('/signup', (req, res) => {
   // If the user is already logged in, redirect the request to another route
